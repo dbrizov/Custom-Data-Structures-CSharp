6c6f914ecb295ab4bd721eeb42af25ec3f6c4360
Denis Rizov <d.b.rizov@gmail.com>
2013-05-26 11:17:55 +0300

    CustomStack.cs added
    CustomStack (implemented and tested)

---
+++ CustomDataStructures/CustomDataStructures.Tests/CustomStackTests.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace CustomDataStructures.Tests
+{
+    [TestClass]
+    public class CustomStackTests
+    {
+        [TestMethod]
+        public void TestPush_FiveItems()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 5;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            Assert.AreEqual(stackCount, stack.Count);
+            Assert.AreEqual("43210", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestPush_TenItems()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 10;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            Assert.AreEqual(stackCount, stack.Count);
+            Assert.AreEqual("9876543210", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestPeek_NonEmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 5;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            Assert.AreEqual(4, stack.Peek());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestPeek_EmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            Assert.AreEqual(4, stack.Peek());
+        }
+
+        [TestMethod]
+        public void TestPop_OneItem()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 5;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            int popped = stack.Pop();
+            Assert.AreEqual(4, popped);
+            Assert.AreEqual(4, stack.Count);
+            Assert.AreEqual("3210", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestPop_MultipleItems()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 10;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                int popped = stack.Pop();
+                Assert.AreEqual(9 - i, popped);
+            }
+
+            Assert.AreEqual(5, stack.Count);
+            Assert.AreEqual("43210", stack.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestPop_EmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            Assert.AreEqual(4, stack.Pop());
+        }
+
+        [TestMethod]
+        public void TestToString_NonEmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            int stackCount = 10;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            Assert.AreEqual("9876543210", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestToString_EmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            Assert.AreEqual("", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestToArray_EmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int[] arr = stack.ToArray();
+
+            Assert.AreEqual(0, arr.Length);
+        }
+
+        [TestMethod]
+        public void TestToArray_NonEmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 10;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            int[] arr = stack.ToArray();
+
+            Assert.AreEqual(10, arr.Length);
+            Assert.AreEqual("9876543210", string.Join("", arr));
+        }
+
+        [TestMethod]
+        public void TestClone_EmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            CustomStack<int> clone = (CustomStack<int>)stack.Clone();
+
+            int count = 5;
+            for (int i = 0; i < count; i++)
+            {
+                stack.Push(i);
+            }
+
+            Assert.AreEqual(5, stack.Count);
+            Assert.AreEqual(0, clone.Count);
+            Assert.AreEqual("", clone.ToString());
+        }
+
+        [TestMethod]
+        public void TestClone_NonEmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 5;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            CustomStack<int> clone = (CustomStack<int>)stack.Clone();
+
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Pop();
+            }
+
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(5, clone.Count);
+            Assert.AreEqual("43210", clone.ToString());
+        }
+
+        [TestMethod]
+        public void TestClear_EmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            stack.Clear();
+
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual("", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestClear_NonEmptyStack()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            int stackCount = 5;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            stack.Clear();
+
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual("", stack.ToString());
+        }
+
+        [TestMethod]
+        public void TestForeachLoop_Enumerator()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            int stackCount = 5;
+            for (int i = 0; i < stackCount; i++)
+            {
+                stack.Push(i);
+            }
+
+            string expected = "43210";
+            StringBuilder actual = new StringBuilder();
+
+            foreach (var item in stack)
+            {
+                actual.Append(item.ToString());
+            }
+
+            Assert.AreEqual(expected, actual.ToString());
+        }
+    }
+}
---
+++ CustomDataStructures/CustomDataStructures/CustomStack.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDataStructures
+{
+    public class CustomStack<T> : IEnumerable<T>, ICloneable
+    {
+        private class Node
+        {
+            public T Item { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T item)
+            {
+                this.Item = item;
+                this.Next = null;
+            }
+
+            public Node(T item, Node next)
+            {
+                this.Item = item;
+                this.Next = next;
+            }
+        }
+
+        private Node top;
+        private int count;
+
+        public CustomStack()
+        {
+            this.top = null;
+            this.count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public T Peek()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return this.top.Item;
+        }
+
+        public T Pop()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T result = this.top.Item;
+            this.top = this.top.Next;
+            this.count--;
+
+            return result;
+        }
+
+        public void Push(T item)
+        {
+            if (this.top == null)
+            {
+                // We have empty stack
+                this.top = new Node(item);
+            }
+            else
+            {
+                Node newNode = new Node(item, this.top);
+                this.top = newNode;
+            }
+
+            this.count++;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            Node currentNode = this.top;
+            while (currentNode != null)
+            {
+                result.Append(currentNode.Item.ToString());
+                currentNode = currentNode.Next;
+            }
+
+            return result.ToString();
+        }
+
+        public T[] ToArray()
+        {
+            T[] result = new T[this.count];
+
+            Node currentNode = this.top;
+            for (int i = 0; i < this.count; i++)
+            {
+                result[i] = currentNode.Item;
+                currentNode = currentNode.Next;
+            }
+
+            return result;
+        }
+
+        public void Clear()
+        {
+            this.top = null;
+            this.count = 0;
+        }
+
+        public object Clone()
+        {
+            CustomStack<T> clone = new CustomStack<T>();
+
+            if (this.count != 0)
+            {
+                // We have non-empty stack
+                clone.top = new Node(this.top.Item);
+                Node currentNode = this.top.Next;
+                Node helperNode = clone.top; // helps us to make the links between the nodes of the clone
+
+                while (currentNode != null)
+                {
+                    Node newNode = new Node(currentNode.Item);
+                    helperNode.Next = newNode;
+                    helperNode = newNode;
+                    currentNode = currentNode.Next;
+                }
+
+                helperNode.Next = null;
+                clone.count = this.count;
+            }
+
+            return clone;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node currentNode = this.top;
+            while (currentNode != null)
+            {
+                T result = currentNode.Item;
+                currentNode = currentNode.Next;
+
+                yield return result;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
